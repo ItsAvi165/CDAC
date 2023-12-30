ce463b5dce0564e49742b573e71dc2629fcb14f0
Avishkar Hongekar <69665805+ItsAvi165@users.noreply.github.com>
2023-12-30 20:58:43 +0530

    Create PracticePaper1.sql
    Added table creation queries and started solving with questions

---
+++ Database Technologies/PracticePaper1.sql
@@ -0,0 +1,42 @@
+--Queries to create table and insert data
+-- Create Students table
+CREATE TABLE Students (
+    Student_No INT PRIMARY KEY,
+    Forename VARCHAR(50),
+    Surname VARCHAR(50)
+);
+
+-- Insert sample data into Students table
+INSERT INTO Students (Student_No, Forename, Surname)
+VALUES
+    (1, 'John', 'Doe'),
+    (2, 'Jane', 'Smith'),
+    (3, 'Emile', 'Zola');
+
+-- Create Marks table
+CREATE TABLE Marks (
+    Student_No INT,
+    Module_Name VARCHAR(50),
+    Mark INT,
+    FOREIGN KEY (Student_No) REFERENCES Students(Student_No)
+);
+
+-- Insert sample data into Marks table
+INSERT INTO Marks (Student_No, Module_Name, Mark)
+VALUES
+    (1, 'Math', 90),
+    (1, 'Science', 75),
+    (2, 'Math', 85),
+    (2, 'Science', 70),
+    (3, 'Math', 95),
+    (3, 'Science', 80);
+
+
+
+-- Question 1. DISPLAY STUDENT NO. AND TOTAL MARKS OF EACH STUDENT HAVING TOTAL MARKS GREATER 
+-- THAN 160.
+
+ select students.Student_No, SUM(Mark) as TOTAL_MARKS 
+ from students inner join marks 
+ on students.Student_No=marks.Student_No 
+ group by students.Student_No having SUM(marks.Mark)>160;
