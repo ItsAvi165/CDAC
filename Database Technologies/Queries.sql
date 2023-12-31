6adc240a6f92467d27d1f0006427746dc6a9af63
ItsAvi165 <avishkar.hongekar@gmail.com>
2023-12-31 23:07:06 +0530

    added another file

---
+++ Database Technologies/Queries.sql
@@ -0,0 +1,10 @@
+create view myview as SELECT * from Marks;
+
+create index myindex on Marks(Student_No);
+
+
+create procedure getmaxmarks(OUT top int)
+  BEGIN
+  select max(Mark) into top from Marks;
+  END &&
+  delimiter ;
