b734b76e42397a51d57c0d92af3bd85996de9924
ItsAvi165 <avishkar.hongekar@gmail.com>
2023-12-31 14:14:55 +0530

    Added one more practie paper

--- Database Technologies/AdditionalQueries.sql
+++ Database Technologies/AdditionalQueries.sql
@@ -7,7 +7,49 @@ WHERE   TABLE_NAME LIKE 'T%'
 OR
 
 SHOW TABLES LIKE 'T%'
-
+-------------------------------------------------------------------------------------------------------------------------------------
 --To see stored procedures and functions
 show procedure status where db='dbt';
 show function status where db='dbt';
+
+-------------------------------------------------------------------------------------------------------------------------------------
+
+SELECT FIRST_NAME, LAST_NAME FROM employees WHERE ABS(TIMESTAMPDIFF(YEAR,NOW(),HIRE_DATE)) >
+(SELECT ABS(TIMESTAMPDIFF(YEAR,NOW(),HIRE_DATE)) FROM employees WHERE FIRST_NAME='Clara');
+
+--Unit of TIMESTAMPDIFF can be - FRAC_SECOND (microseconds), SECOND, MINUTE, HOUR, DAY, WEEK, MONTH, QUARTER, or YEAR.
+
+-------------------------------------------------------------------------------------------------------------------------------------
+
+YEAR(HIRE_DATE) AS JOIN_YEAR
+
+SELECT YEAR(HIRE_DATE) AS JOIN_YEAR, COUNT(*) AS EMPLOYEE_COUNT FROM employees
+GROUP BY JOIN_YEAR HAVING EMPLOYEE_COUNT > 10;     --can use aliases in the same query again eg. JOIN_YEAR and EMPLOYEE_COUNT
+-------------------------------------------------------------------------------------------------------------------------------------
+
+SELECT JOB_TITLE,SUBSTRING(JOB_TITLE,1,1) AS FIRST_LETTER_OF_JOB_TITLE FROM jobs
+WHERE (LENGTH(JOB_TITLE) - LENGTH(REPLACE(JOB_TITLE,' ', '')) + 1) > 1;
+
+--OR
+
+ SELECT JOB_TITLE,SUBSTRING(JOB_TITLE,1,1) AS FIRST_LETTER_OF_JOB_TITLE FROM jobs
+ WHERE JOB_TITLE LIKE '% %';
+
+-------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+-------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+-------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+-------------------------------------------------------------------------------------------------------------------------------------
