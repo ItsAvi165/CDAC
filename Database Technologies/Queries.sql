521944cb7b20dd2941c85a177ec44f80c26593b3
ItsAvi165 <avishkar.hongekar@gmail.com>
2024-01-01 11:59:12 +0530

    Added some more queries

--- Database Technologies/Queries.sql
+++ Database Technologies/Queries.sql
@@ -17,4 +17,43 @@ create procedure getmaxmarks(OUT top int)
   call getmaxmarks(@top);
   select @top;
 
--------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------
+
+DECLARE cursor_name CURSOR FOR
+Select statement;
+
+--cursor example
+
+DELIMITER //
+CREATE PROCEDURE ExampleProc()
+   BEGIN
+      DECLARE done INT DEFAULT 0;
+      DECLARE tutorialID INTEGER;
+      DECLARE tutorialTitle, tutorialAuthor, tutorialDate VARCHAR(20);
+      DECLARE cur CURSOR FOR SELECT * FROM tutorials;
+      DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+      OPEN cur;
+      label: LOOP
+      FETCH cur INTO tutorialID, tutorialTitle, tutorialAuthor, tutorialDate;
+      INSERT INTO backup VALUES(tutorialID, tutorialTitle, tutorialAuthor, tutorialDate);
+      IF done = 1 THEN LEAVE label;
+      END IF;
+      END LOOP;
+      CLOSE cur;
+   END//
+DELIMITER ;
+
+--------------------------------------------------------------------------------------------------------------
+--OR operation in MongoDB
+
+db.collection.find({$or:[{roll:{$lt:6},marks:{$gt:65}}]})
+
+db.runCommand({distinct:'student',key:'address'})   --student = collection name
+
+db.collection.find({roll:{$in:[2,4,6]}})
+
+db.collection.find({marks:{$not:{$gt:60}}})
+
+db.collection.find({marks:{$exists:true}})
+
+db.remove({college:{$exists:true}})
